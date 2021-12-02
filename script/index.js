65e8e53561b13e8827f09e90d19a2c0e1f6b39d7
mdanieladla <mariadanieladarnea@gmail.com>
2021-12-02 20:15:24 +0100

    changed code js

--- script/index.js
+++ script/index.js
@@ -1,19 +1,38 @@
 'use strict';
-//*****************active for mobile ******************//
-const normalDiv = document.querySelector('.js-normal');
+// //*****************active for mobile ******************//
+// const normalDiv = document.querySelector('.js-normal');
 
-const activeDiv = document.querySelector('.js-active');
+// const activeDiv = document.querySelector('.js-active');
 
-const shareBtn = document.querySelectorAll('.js-btn');
+// const shareBtn = document.querySelectorAll('.js-btn');
 
-function handleClickBtn() {
-  if (activeDiv.classList.contains('hidden')) {
-    activeDiv.classList.toggle('hidden');
-    normalDiv.classList.toggle('hidden');
-  } else {
-    activeDiv.classList.toggle('hidden');
-    normalDiv.classList.toggle('hidden');
+// function handleClickBtn() {
+//   if (activeDiv.classList.contains('hidden')) {
+//     activeDiv.classList.toggle('hidden');
+//     normalDiv.classList.toggle('hidden');
+//   } else {
+//     activeDiv.classList.toggle('hidden');
+//     normalDiv.classList.toggle('hidden');
+//   }
+// }
+
+// shareBtn.forEach((n) => n.addEventListener('click', handleClickBtn));
+
+//store .share and .share-img in variables
+const shareHtml = document.querySelector('.js-share');
+
+const shareIcon = document.querySelector('.js-share-img');
+
+//listen for click on icon
+shareIcon.addEventListener('click', (ev) => {
+  //within listener
+  //show/hide correct share menu
+  //avoid shutting the sahre popup when clicking on it
+  if (ev.target === shareIcon) {
+    if (window.innerWidth <= 750) {
+      shareHtml.classList.toggle('active-share-mobile');
+      return;
+    }
+    shareHtml.classList.toggle('active-share-desktop');
   }
-}
-
-shareBtn.forEach((n) => n.addEventListener('click', handleClickBtn));
+});
